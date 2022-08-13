a61dfc37affc5a3dffd34c8559077f8f61df59b3
Larissa Keliany Oliveira <loliveira@mcfadyen.com>
2022-08-13 10:12:18 -0300

    node_modules be gone!

--- server/app.js
+++ server/app.js
@@ -3,16 +3,18 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
-mongoose.connect(process.env.DB);
+mongoose.connect(process.env.DB_URL);
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB Database');
 } )
 
 
 const app = express();
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
